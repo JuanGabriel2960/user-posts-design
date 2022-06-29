bb630c5bc938fc2d76b9cbfa60876d622ebc83b3
Juan Gabriel Tavárez López <juangabriel2960@gmail.com>
2022-06-29 02:02:03 -0400

    feat: added auth context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { Navigation } from './router/Navigation';
+import { AuthProvider } from './context/auth/AuthContext';
 import './normalize.css';
+
+const AppState = ({ children }: any) => {
+  return (
+    <AuthProvider>
+      {children}
+    </AuthProvider>
+  )
+}
 
 function App() {
   return (
-    <>
+    <AppState>
       <Navigation />
-    </>
+    </AppState>
   );
 }
 
--- src/router/Navigation.tsx
+++ src/router/Navigation.tsx
@@ -1,16 +1,25 @@
-import { Suspense } from 'react';
+import { Suspense, useContext } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthModule, UserModule } from './routes';
+import { AuthContext } from '../context/auth/AuthContext';
 
 export const Navigation = () => {
+
+    const { status } = useContext(AuthContext);
+
     return (
         <Suspense fallback={<span>Loading...</span>}>
             <BrowserRouter>
                 <Routes>
-
-                    <Route path='/auth/*' element={<AuthModule />} />
-                    <Route path='/user/*' element={<UserModule />} />
-
+                    {
+                        (status !== 'authenticated')
+                            ? (
+                                <Route path='/auth/*' element={<AuthModule />} />
+                            )
+                            : (
+                                <Route path='/user/*' element={<UserModule />} />
+                            )
+                    }
                     <Route path='/*' element={<Navigate to='/auth/*' />} />
                 </Routes>
             </BrowserRouter>
