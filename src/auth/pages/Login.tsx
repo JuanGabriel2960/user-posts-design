6c6dc52bc3006aefdce8e7322f28eb6ab6f2873d
Juan Gabriel Tavárez López <juangabriel2960@gmail.com>
2022-06-30 16:30:18 -0400

    feat: added login

--- src/auth/pages/Login.tsx
+++ src/auth/pages/Login.tsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import { FormEvent, useContext, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
+import { AuthContext } from '../../context/auth/AuthContext';
+import { User } from '../../interfaces/users';
+import Swal from 'sweetalert2';
+import http from '../../api/http';
+
+const formValidations = {
+  email: [(value: string) => /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value), 'The email is not valid'],
+}
 
 export const Login = () => {
+
+  const [formSubmitted, setFormSubmitted] = useState(false)
+
+  const { setUser } = useContext(AuthContext);
+
+  const { email, onChange, isFormValid, emailValid, } = useForm({
+    email: '',
+  }, formValidations)
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setFormSubmitted(true)
+
+    if (!isFormValid) return;
+
+    try {
+      const { data } = await http.get<User[]>(`/users?email=${email}`, {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+        }
+      })
+
+      if (!data.length) {
+        return Swal.fire({ title: 'Error', text: 'The credentials are not correct.', icon: 'error', confirmButtonColor: '#ee4865' })
+      }
+
+      setUser(data[0])
+    } catch (error: any) {
+      Swal.fire({ title: 'Error', text: 'An error ocurred. Please try again later.', icon: 'error', confirmButtonColor: '#ee4865' })
+    }
+  }
+
   return (
-    <div>Login</div>
+    <div className="w-11/12 mx-auto md:max-w-3xl">
+      <div className="text-center mb-10 md:mb-14">
+        <h1 className="text-font-strong text-5xl font-bold">Login</h1>
+        <p className="text-font-light text-2xl max-w-xs mx-auto">Get access to your own posts by logging into your account</p>
+      </div>
+      <form autoComplete="off" onSubmit={(e) => handleSubmit(e)}>
+        <div>
+          <div className={`border flex justify-between items-center rounded-lg py-4 px-6 md:py-5 ${(!!emailValid && formSubmitted) && 'border-red-400'}`}>
+            <input type="email" className="bg-transparent text-2xl w-full mr-6" placeholder="Email" id="email"
+              name="email" onChange={(e) => onChange(e.target.value, 'email')} value={email} />
+          </div>
+          <span className="text-red-400 text-xl">{formSubmitted && emailValid}</span>
+        </div >
+        <button
+          className="bg-accent text-white mt-10 py-5 font-bold rounded-lg w-full md:py-6 md:mt-14">Sign in</button>
+        <p className="text-font-light mt-5 text-2xl max-w-md mx-auto text-center md:mt-7">Don't have an account yet? <NavLink to='/auth/register'><span className='text-accent font-medium'>Sign up</span></NavLink></p>
+      </form >
+    </div >
   )
 }
