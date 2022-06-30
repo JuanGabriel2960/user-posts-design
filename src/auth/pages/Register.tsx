4cc1ce53d8e8dfb0f118da1e6e0c9e9e8d3a6892
Juan Gabriel Tavárez López <juangabriel2960@gmail.com>
2022-06-29 21:23:38 -0400

    feat: added form validators

--- src/auth/pages/Register.tsx
+++ src/auth/pages/Register.tsx
@@ -1,23 +1,34 @@
-import { FormEvent, useContext } from 'react';
+import { FormEvent, useContext, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useForm } from '../../hooks/useForm';
 import { AuthContext } from '../../context/auth/AuthContext';
 import { User } from '../../interfaces/users';
 import http from '../../api/http';
 
+const formValidations = {
+  name: [(value: string) => value.length >= 1, 'The name is required'],
+  gender: [(value: string) => value === 'male' || value === 'female', 'The gender is required'],
+  email: [(value: string) => /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value), 'The email is not valid'],
+}
+
 export const Register = () => {
+
+  const [formSubmitted, setFormSubmitted] = useState(false)
 
   const { setUser } = useContext(AuthContext);
 
-  const { name, gender, email, status, onChange } = useForm({
+  const { name, gender, email, status, onChange, isFormValid, nameValid, genderValid, emailValid, } = useForm({
     name: '',
     gender: '',
     email: '',
     status: 'active',
-  })
+  }, formValidations)
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setFormSubmitted(true)
+
+    if (!isFormValid) return;
 
     try {
       const { data } = await http.post<User>('/users', { name, gender, email, status }, {
@@ -28,6 +39,7 @@ export const Register = () => {
 
       setUser(data)
     } catch (error: any) {
+      console.log(error.response.data)
       console.log('An error ocurred. Please try again later.')
     }
   }
@@ -40,25 +52,28 @@ export const Register = () => {
       </div>
       <form autoComplete="off" onSubmit={(e) => handleSubmit(e)}>
         <div className="mb-5 md:mb-7">
-          <div className="border flex justify-between items-center rounded-lg py-4 px-6  md:py-5">
+          <div className={`border flex justify-between items-center rounded-lg py-4 px-6 md:py-5 ${(!!nameValid && formSubmitted) && 'border-red-400'}`}>
             <input type="text" className="bg-transparent text-2xl w-full mr-6" placeholder="Name" id="name"
               name="name" onChange={(e) => onChange(e.target.value, 'name')} value={name} />
           </div>
+          <span className="text-red-400 text-xl">{formSubmitted && nameValid}</span>
         </div >
         <div className="mb-5 md:mb-7">
-          <div className="border flex justify-between items-center rounded-lg py-4 px-6 md:py-5">
+          <div className={`border flex justify-between items-center rounded-lg py-4 px-6 md:py-5 ${(!!emailValid && formSubmitted) && 'border-red-400'}`}>
             <input type="email" className="bg-transparent text-2xl w-full mr-6" placeholder="Email" id="email"
               name="email" onChange={(e) => onChange(e.target.value, 'email')} value={email} />
           </div>
+          <span className="text-red-400 text-xl">{formSubmitted && emailValid}</span>
         </div >
         <div>
-          <div className="border flex justify-between items-center rounded-lg py-4 px-6 md:py-5">
+          <div className={`border flex justify-between items-center rounded-lg py-4 px-6 md:py-5 ${(!!genderValid && formSubmitted) && 'border-red-400'}`}>
             <select id="gender" name="gender" onChange={(e) => onChange(e.target.value, 'gender')} defaultValue={""}>
               <option value="" disabled hidden>Gender</option>
               <option value="female">Female</option>
               <option value="male">Male</option>
             </select>
           </div>
+          <span className="text-red-400 text-xl">{formSubmitted && genderValid}</span>
         </div >
         <button
           className="bg-accent text-white mt-10 py-5 font-bold rounded-lg w-full md:py-6 md:mt-14">Sign up</button>
--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,20 +1,46 @@
 
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
-export const useForm = <T extends Object>(initState: T) => {
+export const useForm = <T extends Object>(initState: T, formValidations: any = {}) => {
 
-    const [state, setState] = useState(initState);
+    const [formState, setFormState] = useState<any>(initState);
+    const [formValidation, setFormValidation] = useState<any>({})
+
+    useEffect(() => {
+        createValidators();
+    }, [formState])
+
+    const isFormValid = useMemo(() => {
+        for (const formValue of Object.keys(formValidation)) {
+            if (formValidation[formValue] !== null) return false
+        }
+
+        return true
+    }, [formValidation])
 
     const onChange = (value: string, field: keyof T) => {
-        setState({
-            ...state,
+        setFormState({
+            ...formState,
             [field]: value
         });
     }
 
+    const createValidators = () => {
+        const formCheckedValues: any = {}
+
+        for (const formField of Object.keys(formValidations)) {
+            const [functionxd, errorMessage] = formValidations[formField]
+
+            formCheckedValues[`${formField}Valid`] = functionxd(formState[formField]) ? null : errorMessage;
+        }
+
+        setFormValidation(formCheckedValues)
+    }
+
     return {
-        ...state,
-        form: state,
+        ...formState,
         onChange,
+        ...formValidation,
+        isFormValid
     }
 }
