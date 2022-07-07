56ec6a3c11e502344b6dda18589b825214075250
Juan Gabriel Tavárez López <juangabriel2960@gmail.com>
2022-07-07 13:57:03 -0400

    feat: added logout

--- src/user/pages/Posts.tsx
+++ src/user/pages/Posts.tsx
@@ -2,10 +2,15 @@ import { useContext } from 'react';
 import { PostsContext } from '../../context/posts/PostsContext';
 import { Card, CardTitle, CardBody, CardButton } from '../components/card';
 import { Filters } from '../components/Filters';
+import { FloatButton } from '../components/FloatButton';
 import { PostForm } from '../components/PostForm';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPowerOff } from '@fortawesome/free-solid-svg-icons'
+import { AuthContext } from '../../context/auth/AuthContext';
 
 export const Posts = () => {
 
+  const { removeUser } = useContext(AuthContext);
   const { posts } = useContext(PostsContext);
 
   return (
@@ -26,6 +31,9 @@ export const Posts = () => {
       </div>
 
       <PostForm />
+      <FloatButton title='Logout' onClick={removeUser}>
+        <FontAwesomeIcon icon={faPowerOff} className='text-white h-8 lg:h-8' />
+      </FloatButton>
     </div>
   )
 }
